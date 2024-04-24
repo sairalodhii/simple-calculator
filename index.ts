4567670db81b1117565d555b7c9bedd422e67a18
sairalodhii <sairalodhi.am12@gmail.com>
2024-04-24 13:17:34 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { massage: "Enter first number", type: "number", name: "firstNumber" },
+  { massage: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    massage: "please select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multipliction", "Division"],
+  },
+]); 
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if (answer.operator === "Multipiction") {
+    console.log(answer.firstNumber * answer.secondNumber)
+}else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+}else {
+    console.log("Please select velid number")
+}
